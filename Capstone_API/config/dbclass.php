b0d35fa5984c3569dc9a8e0b7ae12d816eecc138
user-getusername <michael.robertson02@gmail.com>
2019-01-13 21:53:00 -0500

    Added dbclass database config class

---
+++ Capstone_API/config/dbclass.php
@@ -0,0 +1,27 @@
+<?php
+class DBClass {
+
+    private $host = "localhost";
+    private $username = "mrober23_michael";
+    private $password = "password";
+    private $database = "mrober23_php";
+
+    public $connection;
+
+    // get the database connection
+    public function getConnection(){
+
+        $this->connection = null;
+
+        try{
+            $this->connection = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+            $this->connection->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Error: " . $exception->getMessage();
+        }
+
+        return $this->connection;
+    }
+}
+?>

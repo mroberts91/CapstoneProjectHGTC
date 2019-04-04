94aaebfb55e5a3fe03b3aad1f249e652c36fc7eb
MichaelRobertson <michael.robertson@archetypesc.com>
2019-04-03 23:14:30 -0400

    More changes

--- Backend/index.php
+++ Backend/index.php
@@ -6,7 +6,16 @@ require_once __DIR__ . "/includes/header.php";
     Header and footer already contain the outer container div
      which give a margin on the left and right sides of the page.
  -->
-<h1 class="text-center">Jae Tsunami's Admin Portal</h1>
+    <h1 class="text-center">Welcome to the<br>Adminitrative Portal</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div id="home-logo"></div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
 
 <?php
 // This is just here to test to make sure data is being stored in Session variable
